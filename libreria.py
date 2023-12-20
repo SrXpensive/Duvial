1ce62fcac4acf77c997133dd0855c7f07c0f48b5
Edu <eduardo.caro.lorente@gmail.com>
2023-12-20 21:32:29 +0100

    modos de dificultad implementados

--- libreria.py
+++ libreria.py
@@ -2,18 +2,29 @@ import random
 
 operadores = ["+","-","*"]
 capitales = {"España":"Madrid", "Francia":"Paris","Portugal":"Lisboa","Alemania":"Berlin","Austria":"Viena","Bélgica":"Bruselas","Dinamarca":"Copenhague","Bulgaria":"Sofia","Finlandia":"Helsinki","Grecia":"Atenas"}
+capitales2 = {"Macedonia":"Skopje","Bielorrusia":"Minsk","Montenegro":"Podgorica","Albania":"Tirana","Bosnia y Herzegovina":"Sarajevo","Luxemburgo":"Luxemburgo","Moldavia":"Chisinau","Eslovenia":"Liubliana","Lituania":"Vilna","Serbia":"Belgrado"}
 palabras = ["texto","abierto","horno","clase","ancho","equipo","epico","seleccion","raudo"]
+palabras2 = ["electroencefalografista","esternocleidomastoideo","anticonstitucionalidad","desoxirribonucleico","otorrinolaringologo","contrarrevolucionario","interdisciplinario"]
 
-def preguntaNumerica():
-    pregunta = str(random.randint(1,11))+random.choice(operadores)+str(random.randint(1,11))+random.choice(operadores)+str(random.randint(1,11))+random.choice(operadores)+str(random.randint(1,11))
+def preguntaNumerica(d):
+    if d == 1:
+        pregunta = str(random.randint(1,11))+random.choice(operadores)+str(random.randint(1,11))+random.choice(operadores)+str(random.randint(1,11))+random.choice(operadores)+str(random.randint(1,11))
+    elif d == 2:
+        pregunta = str(random.randint(1,11))+random.choice(operadores)+str(random.randint(1,11))+random.choice(operadores)+str(random.randint(1,11))+random.choice(operadores)+str(random.randint(1,11))+random.choice(operadores)+str(random.randint(1,11))+random.choice(operadores)+str(random.randint(1,11))    
     return pregunta
 
-def preguntaCapital():
-    pregunta = random.choice(list(capitales.keys()))
+def preguntaCapital(d):
+    if d == 1:
+        pregunta = random.choice(list(capitales.keys()))
+    elif d == 2:
+        pregunta = random.choice(list(capitales2.keys()))
     return pregunta
 
-def preguntaTexto():
-    palabra = random.choice(palabras)
+def preguntaTexto(d):
+    if d == 1:
+        palabra = random.choice(palabras)
+    elif d == 2:
+        palabra = random.choice(palabras2)
     cadena = ""
     numHuecos = len(palabra) // 3
     palabra2 = list(palabra)
@@ -26,4 +37,5 @@ def preguntaTexto():
         palabra2[numero] = "*"
     for letra in palabra2:
         cadena+=letra
+        
     return cadena, palabra
--- main.py
+++ main.py
@@ -3,9 +3,11 @@ import libreria
 import random
 
 while True:
-    flag = False
+    checkplayers = False
+    checkmode = False
     opcion = "w"
     participantes = {}
+    print("-------------------")
     print("BIENVENIDO A DUVIAL")
     print("-------------------")
     print("1. Nueva partida")
@@ -27,12 +29,12 @@ while True:
             print("----------")
             
             if opcion == "1":
-                if flag == True:
+                if checkplayers == True:
                     print("Ya has introducido los jugadores")
                     print("------------------------------")
                     time.sleep(0.5)
                     continue
-                flag = True
+                checkplayers = True
                 jugadores = int(input("Introduce el número de jugadores (máximo 4): "))
                 print("------------------------------------------")
                 while jugadores >= 5:
@@ -45,19 +47,43 @@ while True:
                 print("------------------------------------------")        
 
             elif opcion == "2":
-                pass
-            
+                checkmode = True
+                print("----------")
+                print("Elige un nivel de dificultad")
+                print("----------")
+                print("1. Normal")
+                print("2. Desafío (el escalón de dificultad es muy grande, no escojas esta)")
+                print("----------")
+                time.sleep(0.5)
+                dificultad = int(input("¿Cuál es tu decisión?: "))
+                if dificultad == 1:
+                    print("Sabia elección")
+                elif dificultad == 2:
+                    print("No sabes lo que haces...")
+                        
+                time.sleep(0.5)
+                
             elif opcion =="3":
                 if not participantes:
                     print("---------------------")
                     print("Primero debes introducir el número de jugadores")
                     print("---------------------")
-                    break
+                    continue
+                if checkmode == False:
+                    print("---------------------")
+                    print("Primero debes escoger un nivel de dificultad")
+                    print("---------------------")
+                    continue
+                
                 print("COMIENZA EL JUEGO")
                 print("-----------------")
                 players = list(participantes.keys())
                 random.shuffle(players)
-                rondas = int(input("¿Cuantas rondas queréis jugar?: "))
+                rondas = int(input("¿Cuantas rondas queréis jugar?(mínimo 3): "))
+                if rondas < 3:
+                    rondas = 3
+                    print("Vas a jugar 3 rondas igualmente")
+                    time.sleep(0.5)
                 print("------------------------")
                 for i in range(1,rondas+1):
                     time.sleep(0.5)
@@ -74,39 +100,57 @@ while True:
                             
                         if eleccion == 1:
                             time.sleep(0.5)
-                            pregunta = libreria.preguntaNumerica()
+                            pregunta = libreria.preguntaNumerica(dificultad)
                             print("Resuelve: ",pregunta)
                             time.sleep(0.5)
                             respuesta = int(input("Solución: "))
                             if respuesta == eval(pregunta):
-                                print("Correcto!")
+                                print("¡Correcto!")
                                 participantes[jugador] += 1
                             else:
                                 print("Respuesta errónea")
+                                time.sleep(0.5)
+                                print("La respuesta correcta era",eval(pregunta))
                                     
                         elif eleccion == 2:
                             time.sleep(0.5)
-                            pregunta = libreria.preguntaCapital()
+                            pregunta = libreria.preguntaCapital(dificultad)
                             print("¿Cuál es la capital de",pregunta,"?")
                             respuesta = input("Solución:")
-                            if respuesta.lower() == libreria.capitales[pregunta].lower():
-                                print("¡Respuesta correcta!")
-                                participantes[jugador] += 1
-                                print("----------")
-                            else:
-                                print("Respuesta incorrecta")
-                                print("----------")
+                            if dificultad == 1:
+                                if respuesta.lower() == libreria.capitales[pregunta].lower():
+                                    print("¡Respuesta correcta!")
+                                    participantes[jugador] += 1
+                                    print("----------")
+                                else:
+                                    print("Respuesta incorrecta")
+                                    time.sleep(0.5)
+                                    print("La respuesta correcta era",libreria.capitales[pregunta])
+                                    print("----------")
+                            elif dificultad == 2:
+                                if respuesta.lower() == libreria.capitales2[pregunta].lower():
+                                    print("¡Respuesta correcta!")
+                                    participantes[jugador] += 1
+                                    print("----------")
+                                else:
+                                    print("Respuesta incorrecta")
+                                    time.sleep(0.5)
+                                    print("La respuesta correcta era",libreria.capitales2[pregunta])
+                                    print("----------")
                                     
                         elif eleccion ==3:
-                            palabra,solucion = libreria.preguntaTexto()
+                            palabra,solucion = libreria.preguntaTexto(dificultad)
                             print(palabra)
                             time.sleep(0.5)
                             respuesta = input("Resuelve la palabra: ")
                             time.sleep(0.5)
                             if respuesta == solucion:
                                 print("¡Correcto!")
+                                participantes[jugador] += 1
                             else:
                                 print("Respuesta incorrecta")
+                                time.sleep(0.5)
+                                print("La respuesta correcta era",solucion)
 
                 if jugadores > 1:
                     print("RESULTADO DE LA PARTIDA:")
@@ -119,7 +163,9 @@ while True:
                     continue
                 else:
                     print("Lo has hecho muy bien",jugador,",has obtenido un total de",participantes[jugador],"puntos")
-                    continue
+                    print("------------------------")
+                    time.sleep(0.5)
+                    break
     elif starter == 2:
         break
     else:
