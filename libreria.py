3be9bd5dfc15d356f093351bb8e528186923aa71
Edu <eduardo.caro.lorente@gmail.com>
2023-12-20 21:58:39 +0100

    funciones comentadas

--- libreria.py
+++ libreria.py
@@ -7,6 +7,9 @@ palabras = ["texto","abierto","horno","clase","ancho","equipo","epico","seleccio
 palabras2 = ["electroencefalografista","esternocleidomastoideo","anticonstitucionalidad","desoxirribonucleico","otorrinolaringologo","contrarrevolucionario","interdisciplinario"]
 
 def preguntaNumerica(d):
+    '''
+    Función para generar la pregunta numérica. Alterna entre carácteres numéricos obtenidos aleatoriamente entre 1 y 10, y operadores matemáticos extraídos de la lista "operadores". Concatena lo anterior para generar una expresión. Dependiendo de la dificultad añade más o menos términos en la expresión.
+    '''
     if d == 1:
         pregunta = str(random.randint(1,11))+random.choice(operadores)+str(random.randint(1,11))+random.choice(operadores)+str(random.randint(1,11))+random.choice(operadores)+str(random.randint(1,11))
     elif d == 2:
@@ -14,6 +17,9 @@ def preguntaNumerica(d):
     return pregunta
 
 def preguntaCapital(d):
+    '''
+    Función para generar la pregunta de capitales. Disponemos de dos diccionarios formados por pares de paises:capitales. Dependiendo del nivel de dificultad escogido, las capitales serán más sencillas o más complicadas de acertar.
+    '''
     if d == 1:
         pregunta = random.choice(list(capitales.keys()))
     elif d == 2:
@@ -21,6 +27,9 @@ def preguntaCapital(d):
     return pregunta
 
 def preguntaTexto(d):
+    '''
+    Función para generar la pregunta de texto. Accedemos a una lista de palabras para obtener una de ellas aleatoriamente. Dependiendo de la dificultad seleccionada se accederá a una lista de palabras más cortas o más largas.
+    '''
     if d == 1:
         palabra = random.choice(palabras)
     elif d == 2:
